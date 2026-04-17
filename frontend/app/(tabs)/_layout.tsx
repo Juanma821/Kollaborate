195936c008ab56d0ce1a5ccda893fbdec1f6b584
dbdominguez <d.dominguezg@duocuc.cl>
2026-04-17 16:54:33 -0400

    Feat: Actualización navegación entre pantallas

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -10,7 +10,8 @@ export default function TabsLayout() {
            title: 'Salon', 
            tabBarIcon: ({ color, focused }) => (<Ionicons name={focused ? 'easel-sharp' : 'easel-outline'} color={color} size={24} />),   }} />
 
-        <Tabs.Screen name="search" options={{
+        <Tabs.Screen name="search" listeners={({ navigation }) => ({
+           tabPress: (e) => {navigation.navigate('search', { screen: 'index' });},})} options={{
            headerShown: false, 
            title: 'Busqueda', 
            tabBarIcon: ({ color, focused }) => (<Ionicons name={focused ? 'search-sharp' : 'search-outline'} color={color} size={24} />),   }} />
@@ -21,12 +22,14 @@ export default function TabsLayout() {
            title: 'Inicio', 
            tabBarIcon: ({ color, focused }) => (<Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />),  }} />
 
-        <Tabs.Screen name="mailbox" options={{
+        <Tabs.Screen name="mailbox" listeners={({ navigation }) => ({
+           tabPress: (e) => {navigation.navigate('mailbox', { screen: 'index' });},})} options={{
            headerShown: false, 
            title: 'Buzon', 
            tabBarIcon: ({ color, focused }) => (<Ionicons name={focused ? 'chatbubbles-sharp' : 'chatbubbles-outline'} color={color} size={24} />),   }} />
 
-        <Tabs.Screen name="profile" options={{
+        <Tabs.Screen name="profile" listeners={({ navigation }) => ({
+          tabPress: (e) => {navigation.navigate('profile', { screen: 'index' });},})} options={{
            headerShown: false, 
            title: 'Perfil', 
            tabBarIcon: ({ color, focused }) => (<Ionicons name={focused ? 'person-sharp' : 'person-outline'} color={color} size={24} />),   }} />
