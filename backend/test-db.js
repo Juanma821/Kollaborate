7344f0d693d29847ebbd01422748d35b15dfb932
rsaezt <ricardoandressaeztapia@gmail.com>
2026-04-18 20:15:47 -0400

    actualizacion backend

---
+++ backend/test-db.js
@@ -0,0 +1,25 @@
+const oracledb = require('oracledb');
+
+process.env.TNS_ADMIN = "C:/Users/gonga/Desktop/Portafolio/conexiondb";
+
+async function testConnection() {
+    try {
+        const connection = await oracledb.getConnection({
+            user: 'Usuario2',
+            password: 'DuocUC123456',
+            connectString: 'kldhz48x2nkrpwvg_high'
+        });
+
+        console.log('✅ Conectado a Oracle');
+
+        const result = await connection.execute(`SELECT * FROM DUAL`);
+        console.log(result.rows);
+
+        await connection.close();
+
+    } catch (err) {
+        console.error('❌ Error:', err);
+    }
+}
+
+testConnection();
